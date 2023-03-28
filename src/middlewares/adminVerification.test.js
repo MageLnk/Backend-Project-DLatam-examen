4d757c699383dbdeba430d2433a16b3fbfe67cf8
MageLink <mage.link.zhezard@gmail.com>
2023-03-28 12:34:05 -0300

    Minnor add to a one  test

--- src/middlewares/adminVerification.test.js
+++ src/middlewares/adminVerification.test.js
@@ -5,8 +5,7 @@ describe("Given adminVerification function", () => {
   let req;
   let res;
   let next;
-  const secretKey =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFkbWluIiwiZW1haWwiOiJhZG1pbkB0ZXN0LnRlc3QiLCJ0eXBlIjoiYWRtaW4iLCJpYXQiOjE2Nzk5NjU4MDJ9.PxhWaaGeeYXLA74cV1S0yeP2u3oTnqN3iaGIggw0dsg";
+  const secretKey = "TestSecretKey";
   beforeEach(() => {
     req = {
       headers: {},
@@ -16,6 +15,10 @@ describe("Given adminVerification function", () => {
       send: jest.fn(),
     };
     next = jest.fn();
+  });
+
+  afterEach(() => {
+    jest.resetAllMocks();
   });
 
   test("It should return 403 if authorization header is missing", async () => {
